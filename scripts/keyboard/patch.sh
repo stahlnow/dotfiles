756d80caf5db7c98c81cde84fbc877d2172138af
stahl <office@stahlnow.com>
2022-06-29 14:59:15 +0200

    absolute paths

--- scripts/keyboard/patch.sh
+++ scripts/keyboard/patch.sh
@@ -3,8 +3,8 @@
 # diff -u /usr/share/X11/xkb/symbols/ch ch.modified > ch.diff
 # sudo patch /usr/share/X11/xkb/symbols/ch < ch.diff
 
-diff -u /usr/share/X11/xkb/symbols/pc pc.modified > pc.diff
-sudo patch /usr/share/X11/xkb/symbols/pc < pc.diff
+diff -u /usr/share/X11/xkb/symbols/pc ~/.dotfiles/scripts/keyboard/pc.modified > ~/.dotfiles/scripts/keyboard/pc.diff
+sudo patch /usr/share/X11/xkb/symbols/pc < ~/.dotfiles/scripts/keyboard/pc.diff
 
 # restart keyboard
 # setxkbmap ch
