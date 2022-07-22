f95a63db4b10113bb0b99820146142583d3edb20
stahl <office@stahlnow.com>
2022-07-22 15:02:42 +0200

    changed keyboard settings

--- scripts/keyboard/patch.sh
+++ scripts/keyboard/patch.sh
@@ -1,16 +1,7 @@
 #!/bin/zsh
-
-# diff -u /usr/share/X11/xkb/symbols/ch ch.modified > ch.diff
-# sudo patch /usr/share/X11/xkb/symbols/ch < ch.diff
-
 diff -u /usr/share/X11/xkb/symbols/pc ~/.dotfiles/scripts/keyboard/pc.modified > ~/.dotfiles/scripts/keyboard/pc.diff
 sudo patch /usr/share/X11/xkb/symbols/pc < ~/.dotfiles/scripts/keyboard/pc.diff
-
 # restart keyboard
-# setxkbmap ch
 setxkbmap us
-
 # restart window manager
 swaymsg reload
-
-
