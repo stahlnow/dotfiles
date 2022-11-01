513acb086c115fe5bc58486feb291c6dd349b02b
stahl <office@stahlnow.com>
2022-11-01 16:55:44 +0100

    lf, marker, sway

--- waybar/launch.sh
+++ waybar/launch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 CONFIG_FILES="$HOME/.config/waybar/config $HOME/.config/waybar/style.css $HOME/.config/waybar/colors.css"
 
-trap "killall waybar" EXIT
-
+#trap "killall waybar" EXIT
+killall waybar
 while true; do
     waybar &
     inotifywait -e create,modify $CONFIG_FILES
