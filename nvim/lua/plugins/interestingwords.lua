643736f7823044dd292669e39bc68ccceeb0403b
stahl <office@stahlnow.com>
2025-09-16 05:17:56 +0200

    lint

--- nvim/lua/plugins/interestingwords.lua
+++ nvim/lua/plugins/interestingwords.lua
@@ -1,8 +1,9 @@
+-- https://github.com/lfv89/vim-interestingwords
 return {
-   "lfv89/vim-interestingwords",
-   config = function()
-      vim.g.interestingWordsGUIColors = {'#8CCBEA', '#A4E57E', '#FFDB72', '#82F8CB', '#D55D87', '#FF977B'};
-      vim.g.interestingWordsRandomiseColors = true;
-      vim.keymap.set('n', '<leader>j', ':call UncolorAllWords()<cr>', { noremap = true  })
-   end,
+	"lfv89/vim-interestingwords",
+	config = function()
+		vim.g.interestingWordsGUIColors = { "#8CCBEA", "#A4E57E", "#FFDB72", "#82F8CB", "#D55D87", "#FF977B" }
+		vim.g.interestingWordsRandomiseColors = true
+		vim.keymap.set("n", "<leader>j", ":call UncolorAllWords()<cr>", { noremap = true })
+	end,
 }
