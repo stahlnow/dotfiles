8ef12f34bc76b8a4b211571c32d41692dee56722
stahl <office@stahlnow.com>
2025-10-26 01:43:12 +0200

    eager load fugitive

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -2,6 +2,7 @@
 -- https://github.com/tpope/vim-fugitive
 return {
 	"tpope/vim-fugitive",
+	lazy = false,
 	keys = {
 		{ "<leader>G", "<cmd>Git<cr>", desc = "Git status, usage :help fugitive" },
 	},
